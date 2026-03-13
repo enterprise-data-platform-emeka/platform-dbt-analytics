06c38edebd759713fec98430874bca3532f63578
Edeh Emeka N. <blessedemeks@gmail.com>
2026-03-13 09:58:49 +0100

    Fix customer_segments: replace USING with ON join for Athena compatibility

--- models/marts/customer/customer_segments.sql
+++ models/marts/customer/customer_segments.sql
@@ -11,8 +11,8 @@ with customer_metrics as (
         min(o.order_date)               as first_order_date,
         max(o.order_date)               as last_order_date
     from {{ ref('stg_customers') }}   c
-    left join {{ ref('stg_orders') }}   o using (customer_id)
-    left join {{ ref('stg_payments') }} p using (order_id)
+    left join {{ ref('stg_orders') }}   o on c.customer_id = o.customer_id
+    left join {{ ref('stg_payments') }} p on o.order_id    = p.order_id
     group by 1, 2, 3, 4, 5, 6
 )
 select
